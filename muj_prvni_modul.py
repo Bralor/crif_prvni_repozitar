5d4c6f359e06f4edcdccd96dd606ce2d517ecd63
Matous <bralor92@email.cz>
2022-05-27 10:46:47 +0200

    Do modulu 'muj_prvni_modul.py' přidáno dnešní datum

--- muj_prvni_modul.py
+++ muj_prvni_modul.py
@@ -2,4 +2,6 @@ import datetime
 
 nyni = datetime.datetime.now()
 
-print(f"Nyní máme: {nyni.strftime('%H:%M:%S')}")
+# Nyní máme: 10:32:04
+# to-do: # Nyní máme: 27/05/2022, 10:32:04
+print(f"Nyní máme: {nyni.strftime('%d/%m/%Y, %H:%M:%S')}")
